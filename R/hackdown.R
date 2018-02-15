6546458ed501a08d45840361732fa5145821e785
Nick Rivera <35855731+Nick-Rivera@users.noreply.github.com>
2018-02-14 21:29:13 -0800

    Fetching and building vignettes now works

--- R/hackdown.R
+++ R/hackdown.R
@@ -18,16 +18,22 @@ get_package_reference_files <- function()
   # untar all
   mapply(untar, tarfile=packages[,2], exdir=out)
   
-  unlink("content/pages/package", recursive = TRUE)
+  unlink("content/page/package", recursive = TRUE)
   
   for (pkg in pkgs) {
     
     exdir=file.path(out, packages[pkg,1])
     
-    pkgdown_outdir <- file.path(getwd(), "content", "page", "package", pkg)
+    # Put the reference pages and vignettes in their own folders under the main package folder.
+    pkgdown_outdir_reference <- file.path(getwd(), "content", "page", "package", pkg, "reference")
+    pkgdown_outdir_vignettes <- file.path(getwd(), "content", "page", "package", pkg)
+    
     system(sprintf("cp -r _pkgdown.yml pkgdown_templates/* %s", exdir))
     
-    pkgdown::build_reference(exdir[1],  path=pkgdown_outdir)
+    # We want pkgdown to build the references, but do not let it build the vignettes.
+    # Blogdown will take care of them, so just copy them over without touching them.
+    pkgdown::build_reference(exdir[1],  path=pkgdown_outdir_reference)
+    system(sprintf("cp -r %s %s", file.path(exdir, "vignettes"), pkgdown_outdir_vignettes))
     
   }
 }

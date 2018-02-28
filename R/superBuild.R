39959990af935ad44aab74199dbe43dd4d9b1ee3
Nick Rivera <35855731+Nick-Rivera@users.noreply.github.com>
2018-02-27 22:31:14 -0800

    New location of files

--- R/superBuild.R
+++ R/superBuild.R
@@ -3,7 +3,7 @@ source("R/hackdown.R")
 get_package_reference_files()
 
 index_pages <-
-  list.files("content/page/package",
+  list.files("content/R",
              pattern = "index.html",
              full.names = TRUE,
              recursive = TRUE)

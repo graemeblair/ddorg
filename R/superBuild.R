5ff714811ebda6bfdd99d658c61da2b136f17523
Nick Rivera <35855731+Nick-Rivera@users.noreply.github.com>
2018-03-04 22:47:03 -0800

    Revert "Relative links for some pages"
    This reverts commit 50869b154471dec1e508348a33f9b0f144ebd367.

--- R/superBuild.R
+++ R/superBuild.R
@@ -2,35 +2,22 @@ source("R/hackdown.R")
 
 get_package_reference_files()
 
-reference_index_pages <-
+index_pages <-
   list.files("content/R",
-             pattern = "index.html",
+             pattern = "^index.html",
              full.names = TRUE,
              recursive = TRUE)
 
 # Rename the reference index pages because right now they are named index.html.
 # index.html files have a special meaning to Hugo. Leaving reference index pages
 # as index.html will mess up Hugo's build process.
-sapply(reference_index_pages, function(file_name)
+sapply(index_pages, function(file_name)
 {
  file.rename(
     from = file_name,
-    to = sub(pattern = "index.html", replacement = "README.html", file_name)
+    to = sub(pattern = "index.html", replacement = "home.html", file_name)
   )
 })
 
 blogdown::build_site()
 
-all_index_pages <-
-  list.files("public/R",
-             pattern = "readme.html",
-             full.names = TRUE,
-             recursive = TRUE)
-
-sapply(all_index_pages, function(file_name)
-{
-  file.rename(
-    from = file_name,
-    to = sub(pattern = "readme.html", replacement = "index.html", file_name)
-  )
-})

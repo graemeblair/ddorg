7c5645276b051bba9547d0574cad2f2a73fa5512
Nick Rivera <35855731+Nick-Rivera@users.noreply.github.com>
2018-03-24 10:35:07 -0700

    Add links back to sample reference code

--- static/js/code_cleanup.js
+++ static/js/code_cleanup.js
@@ -8,23 +8,25 @@ function fix_code_blocks()
     {
         // Each piece of code on a reference page is inside a span tag. Inside each span tag are input and output
         // blocks of R code. Inside each input and output block is a mess of span tags. We are getting the code from
-        // inside each input and output block and stripping out all its inner tags.
-        const code_block   = jQuery(this);
-        const code_content = code_block.text();
+        // inside each input and output block and stripping out all its inner tags, except link elements. We keep link
+        // elements because they link to other parts of our reference files straight from the sample code.
 
-        // It is important to put a newline between input and output blocks so that adjacent input and output blocks do
-        // not squish together into one unreadable mess.
-        code_block.replaceWith(code_content + "\n");
+        const input_output_block = jQuery(this);
+        const non_link_elements  = input_output_block.find("*").not("a");
+
+        // Move from the most inner element to the most outer element.
+        for (let i = non_link_elements.length - 1; i >= 0; i--)
+        {
+            const element         = jQuery(non_link_elements[i]);
+            const element_content = element.html();
+            element.replaceWith(element_content);
+        }
     });
 
+    // Manually trigger code highlighting.
     code_blocks.each(function ()
     {
-        // Now that we have cleaned up the code inside the input/output blocks, strip out the tags that distinguish the
-        // input/output blocks themselves because we do not need those tags anymore.
-        const code_block   = jQuery(this);
-        const code_content = code_block.text();
-
-        code_block.text(code_content.trim());
+        const code_block = jQuery(this);
         code_block.addClass("r");
         hljs.highlightBlock(this);
     });

7c445f9100c874520fe7efcc285dcaa3ad2312c1
Nick Rivera <35855731+Nick-Rivera@users.noreply.github.com>
2018-10-14 22:32:10 -0700

    Local builds
    Closes https://github.com/DeclareDesign/declaredesign.org/issues/70

--- README.md
+++ README.md
@@ -1,3 +1,39 @@
 # DeclareDesign project web site - declaredesign.org
 
 [![Build Status](https://travis-ci.org/DeclareDesign/declaredesign.org.svg?branch=master)](https://travis-ci.org/DeclareDesign/declaredesign.org)
+
+# How do I run the build script locally?
+
+Steps for running locally on macOS:
+
+First, `cd` into the declaredesign.org directory:
+
+```sh
+cd ~/Downloads/declaredesign.org
+```
+
+Then, get your Python environment set up in that directory:
+
+```sh
+# Update your Python packages.
+python3 -m pip install --upgrade pip setuptools wheel virtualenv
+
+# Create a new virtual environment.
+virtualenv venv
+source venv/bin/activate
+
+# Install the required package for the script to run.
+python3 -m pip install pyyaml
+
+```
+
+Next, add your GitHub API token to the top of the file `run_build.py` on the line `GITHUB_API_TOKEN = ""`.
+
+Now, run that scrip with Python:
+
+```sh
+python3 run_build.py
+```
+
+Don't forget that you will also have to install any R packages that are necessary to build the website.
+These are listed in the `DESCRIPTION` file.
---
+++ run_build.py
@@ -0,0 +1,75 @@
+import http.server
+import os
+import socketserver
+import subprocess
+import yaml
+
+GITHUB_API_TOKEN = ""
+PORT = 8000
+
+
+def parse_environment_variables(environment_variable_list):
+    """
+    Turns your list of environment variables into a map of environment variables.
+
+    :param environment_variable_list: A list of environment variables for one package.
+    :return: A map that holds a key-value pair for each environment variable.
+    """
+    environment_variable_map = {}
+
+    for variable in environment_variable_list:
+        variable = variable.split("=", maxsplit=1)
+
+        variable_identifier = variable[0]
+        variable_value = variable[1].strip("'")
+
+        environment_variable_map[variable_identifier] = variable_value
+
+    return environment_variable_map
+
+
+if not GITHUB_API_TOKEN:
+    raise ValueError("Don't forget to add your GitHub API token at the top of this file.")
+
+# Gets a copy of the user's path to give to the build script environment.
+# This makes sure that the build script uses the right programs when it runs (e.g. it looks up the correct version of
+# Node.js or coreutils from the user's path).
+user_environment_variables = os.environ.copy()
+user_path = user_environment_variables["PATH"]
+
+with open(".travis.yml") as travis_yml:
+    travis_environment_variables = yaml.load(travis_yml)
+
+global_environment_variables = travis_environment_variables["env"]["global"]
+package_environment_variables = travis_environment_variables["env"]["matrix"]
+
+# Split up the list of environment variables into one list of environment variables per package.
+package_environment_variables = [x.split(" ") for x in package_environment_variables]
+
+for index, package_to_build in enumerate(package_environment_variables):
+    package_name = parse_environment_variables(package_to_build)["PACKAGE"]
+
+    if not package_name:
+        package_name = "Blog Pages"
+
+    print(f"{index}: {package_name}")
+
+user_choice = input("Which package would you like to build: ")
+package_to_build = package_environment_variables[int(user_choice)]
+
+parsed_global_variables = parse_environment_variables(global_environment_variables)
+parsed_package_variables = parse_environment_variables(package_to_build)
+
+all_variables = {**parsed_global_variables, **parsed_package_variables}
+all_variables["GITHUB_API_TOKEN"] = GITHUB_API_TOKEN
+all_variables["PATH"] = user_environment_variables["PATH"]
+
+subprocess.run(["./download_packages.sh"], env=all_variables)
+
+# Serve the site!
+os.chdir("public/")
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()

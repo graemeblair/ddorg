a64503b8a9008f4aec6c73ab123acc6f6a917f6a
Nick-Rivera <35855731+Nick-Rivera@users.noreply.github.com>
2018-11-07 22:28:22 -0800

    Ignore case when searching for output URL

--- scripts/deploy_previews.sh
+++ scripts/deploy_previews.sh
@@ -3,7 +3,7 @@
 ls -laR ./content
 ls -laR ./public
 
-deploy_url=$(netlify deploy --dir=public | grep 'Live Draft URL')
+deploy_url=$(netlify deploy --dir=public | grep --ignore-case 'Live Draft URL')
 
 # Pass in the API key, pull request number, current package, and our deploy URL.
 node js/post_preview_link.js "$1" "$2" "$3" "$deploy_url"
